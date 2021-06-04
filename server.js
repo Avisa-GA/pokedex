314fcf0091c6d07cdfac65fa9fbff75d3bce8e1d
Avisa-SEI <aap571420@gmail.com>
2021-06-04 16:12:52 -0700

    add: show and index page

--- server.js
+++ server.js
@@ -4,12 +4,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// DATABASE
+const pokemons = require('./models/pokemon.js')
+
 // ROUTE TO START
 app.get('/', (req, res) => {
     res.send('Welcome to Pokedex')
 })
 
+// INDEX
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', {
+        allPokemon: pokemons
+    })
+})
 
+// NEW
+
+// CREATE
+
+// SHOW
+app.get('/pokemon/:index', (req, res) => {
+    res.render('show.ejs', {
+        pokemon: pokemons[req.params.index]
+    })
+})
+
+// DELETE
+
+// EDIT
 
 // LISTEN TO ROUTE
 app.listen(port, () => {
